733106e236c537ec392915e21f459000b7ef48da
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2023-05-25 14:49:31 +0200

    Update zenodo_data_dir cma_test.py

--- cma/tests/cma_test.py
+++ cma/tests/cma_test.py
@@ -15,28 +15,12 @@ from scanpy import read_h5ad
 from importlib_resources import files
 import subprocess
 
-# Import test data
-data_dir="cma/data/training_labels_meta.csv"
-link="https://github.com/Arts-of-coding/cma/blob/develop/cma/data/training_labels_meta.csv"
-command_to_be_executed = ['curl', '-o',  str(data_dir),  str(link)]
-subprocess.check_output(command_to_be_executed, shell=False)
-
-data_dir="cma/data/small_test.h5ad"
-link="https://github.com/Arts-of-coding/cma/blob/develop/cma/data/small_test.h5ad"
-command_to_be_executed = ['curl', '-o',  str(data_dir),  str(link)]
-subprocess.check_output(command_to_be_executed, shell=False)
-
-data_dir="cma/data/cma_meta_atlas.h5ad"
-link="https://github.com/Arts-of-coding/cma/blob/develop/cma/data/cma_meta_atlas.h5ad"
-command_to_be_executed = ['curl', '-o',  str(data_dir),  str(link)]
-subprocess.check_output(command_to_be_executed, shell=False)
-
-query = "cma/data/small_test.h5ad"
-reference="cma/data/cma_meta_atlas.h5ad"
-labels = "cma/data/training_labels_meta.csv"
-od="test_output/"
+query = "small_test.h5ad"
+reference="cma_meta_atlas.h5ad"
+labels = "training_labels_meta.csv"
+outdir="test_output/"
 
 def test_SVM_prediction():
-    command_to_be_executed = ['SVM_prediction', '--reference_H5AD',  str(reference), '--query_H5AD',  str(query), '--LabelsPathTrain',  str(labels), '--OutputDir', str(od)]
+    command_to_be_executed = ['SVM_prediction', '--reference_H5AD',  str(reference), '--query_H5AD',  str(query), '--LabelsPathTrain',  str(labels), '--OutputDir', str(outdir)]
     subprocess.run(command_to_be_executed, shell=False, timeout=None, text=True)
-    assert os.path.exists("test_output/SVM_Pred_Labels.csv") == 1
+    assert os.path.exists(f'{outdir}SVM_Pred_Labels.csv') == 1
